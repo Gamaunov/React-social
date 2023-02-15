7d36467871ff037fc993c68b307e8b528bd99336
Gamaunov <gamaunov1911@gmail.com>
2023-02-15 14:19:48 +0300

    modified app

--- node-app/index.js
+++ node-app/index.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-// const userRoute = require("./routes/users");
-// const authRoute = require("./routes/auth");
-// const postRoute = require("./routes/posts");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -19,15 +19,17 @@ mongoose.connect(
   }
 );
 
-// //middleware
-// app.use(express.json());
-// app.use(helmet());
-// app.use(morgan("common"));
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
-// app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
-// app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
-app.listen(8801, () => {
+
+
+app.listen(8800, () => {
   console.log("Backend server is running!");
 });
--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import Home from './pages/home/Home.jsx'
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./components/profile/Profile";
+import Register from "./pages/register/Register";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  return <Home />;
+  <Router>
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route>
+      <Route path='/register'>
+        <Register />
+      </Route>
+      <Route path='/profile/:username'>
+        <Profile />
+      </Route>
+    </Switch>
+  </Router>
 }
 
 export default App;
